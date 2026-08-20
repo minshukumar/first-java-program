1ef94d8dfeb5e9091600f33f140ef899eee283fa
Minshu <minshuk72@gmail.com>
2026-08-20 19:44:44 +0530

    this is my first java program

---
+++ first.java
@@ -0,0 +1,29 @@
+// // class first {
+// //     public static void main(String args[]){
+// //         System.out.println("minshu hello");
+// //     }
+// // }
+
+
+// class first{
+//     public static void main(String args[]){
+//         System.out.println("*");
+//         System.out.println("* *");
+//         System.out.println("* * *");
+//         System.out.println("* * * *");
+        
+
+//     }
+// } 
+
+import java.util.Scanner;
+
+class first{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter your age");
+        int age = sc.nextInt();
+        System.out.print(age);
+
+    }
+}
