5fc8c83fac754b163a948295f7740d4bb9db6af1
Minshu <minshuk72@gmail.com>
2026-08-20 20:29:51 +0530

    this is second class first program of java

---
+++ second_class.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class second_class {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter age");
+        int age = sc.nextInt();
+
+        if(age>18){
+            System.out.println("young");
+        }
+        else{
+            System.out.println("not young");
+        }
+
+
+    }
+    
+
+    
+}
